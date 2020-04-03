912b98757333d52c3a4e34fcf7ccd0b58fb62dfe
Ilham <i.pirmamadov.synapse.company@gmail.com>
2020-04-03 22:09:44 +0400

    Map Data

---
+++ pages/map.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div v-for="(item,index) in countries" :key="index">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import LeftSidebar from "../components/pages/main-page/LeftSidebar";
+    import NewsList from "../components/pages/main-page/NewsList";
+    import {mapActions, mapState} from 'vuex'
+    import VirusStatic from "../components/global/VirusStatic";
+
+    export default {
+        components: {VirusStatic, NewsList, LeftSidebar},
+        created() {
+            this.getVirus();
+            this.getCountries();
+
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            ...mapActions('virus', ['getVirus']),
+            ...mapActions('virus', ['getCountries']),
+
+        },
+        computed: {
+            ...mapState('virus', ['virusWorldWide']),
+            ...mapState('virus', ['virusLocal']),
+            ...mapState('virus', ['countries'])
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- store/virus.js
+++ store/virus.js
@@ -1,6 +1,7 @@
 export const state = () => ({
     virusWorldWide: null,
     virusLocal: null,
+    countries: null
 })
 
 
@@ -10,6 +11,9 @@ export const mutations = {
     },
     SET_LOCAL_VIRUS: (state, payload) => {
         state.virusLocal = payload
+    },
+    SET_COUNTRIES: (state, payload) => {
+        state.countries = payload
     }
 }
 
@@ -29,4 +33,10 @@ export const actions = {
         commit('SET_VIRUS',virusWorldWide);
         commit('SET_LOCAL_VIRUS',local);
     },
+    async getCountries({commit}) {
+        const countries = await this.$axios.$get('https://covid-19-data.p.rapidapi.com/country/all', {
+            headers: {'x-rapidapi-key': 'c3ae9cdbcfmsh29bc65690a77986p10a54bjsn9bc95c48b530'}
+        });
+        commit('SET_COUNTRIES',countries);
+    }
 }
