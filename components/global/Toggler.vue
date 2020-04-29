2de1dcda1071bf3c9884fa3fbd96a0fe296036b0
rare <chipchroumme@gmail.com>
2020-04-29 21:59:13 +0400

    search on news-page is working (but data and source is missing though)

---
+++ components/global/Toggler.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="toggler">
+    <button class="toggler__button" @click="toggle()" :class="{ active: value }">
+      <div class="toggler__button__toggler"></div>
+    </button>
+    <span class="toggler__text">{{ $t(title) }}</span>
+  </div>
+</template>
+
+<script>
+	export default {
+		props: ['value', 'title'],
+
+		methods: {
+			toggle() {
+				this.$emit('input', !this.value);
+			}
+		}
+	}
+</script>
