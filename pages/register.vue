7e6df9bd5f5714985690e24b61eed1f0ffe65be4
rare <chipchroumme@gmail.com>
2020-05-05 02:52:29 +0400

    form-pages are adaptive

--- pages/register.vue
+++ pages/register.vue
@@ -2,27 +2,29 @@
     <div class="form-page">
         <img class="form-page__bg" data-src="~/static/images/form-page/bg.png" v-lazy-load>
         <div class="form-page__card">
-            <div class="form-page__container">
-                <h1 class="form-page__title">{{ $t('explore') }}</h1>
-            </div>
-            <form class="form-page__form">
+            <div class="form-page__card__content">
                 <div class="form-page__container">
-                    <input class="form-page__input" name="login" type="email" :placeholder="$t('enterLogin')" required>
-                    <input class="form-page__input" name="password" type="password" :placeholder="$t('password')" required>
-                    <input class="form-page__input" name="password-again" type="password" :placeholder="$t('repeatPassword')" required>
-                    <clink :to="'/login'" class="form-page__text double">{{ $t('haveAccount') }} <span class="blue double-second">{{ $t('LogIn') + '!' }}</span></clink>
-                    <div class="form-page__enter-with">
-                        <span class="form-page__text">{{ $t('signInBy') }}</span>
-                        <clink :to="'https://facebook.com/'" class="form-page__social">
-                            <svg-icon name="form-page/fb.svg" />
-                        </clink>
-                        <clink :to="'https://instagram.com/'" class="form-page__social">
-                            <svg-icon name="form-page/ig.svg" />
-                        </clink>
-                    </div>
+                    <h1 class="form-page__title">{{ $t('form-page.register-page.title') }}</h1>
                 </div>
-                <button class="form-page__submit">{{ $t('Зарегистророваться') }}</button>
-            </form>
+                <form class="form-page__form">
+                    <div class="form-page__container">
+                        <input class="form-page__input" name="login" type="email" :placeholder="$t('form-page.enter-login')" required>
+                        <input class="form-page__input" name="password" type="password" :placeholder="$t('form-page.enter-password')" required>
+                        <input class="form-page__input" name="password-again" type="password" :placeholder="$t('form-page.confirm-password')" required>
+                        <clink :to="'/login'" class="form-page__text double">{{ $t('form-page.yes-account') }} <span class="blue double-second">{{ $t('form-page.login') }}!</span></clink>
+                        <div class="form-page__enter-with">
+                            <span class="form-page__text">{{ $t('form-page.enter-with') }}</span>
+                            <clink :to="''" class="form-page__social">
+                                <img src="~/static/svg/form-page/fb.svg" />
+                            </clink>
+                            <clink :to="''" class="form-page__social">
+                                <img src="~/static/svg/form-page/ig.svg" />
+                            </clink>
+                        </div>
+                    </div>
+                    <button class="form-page__submit">{{ $t('form-page.register') }}</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -2,31 +2,33 @@
     <div class="form-page reset">
         <img class="form-page__bg" data-src="~/static/images/form-page/bg.png" v-lazy-load>
         <div class="form-page__card">
-    		<div class="form-page__container">
-	        	<h1 class="form-page__title" v-show="step == 1">{{ $t('usedEmail') }}</h1>
-	        	<h1 class="form-page__title" v-show="step == 2">{{ $t('sentCode') }}</h1>
-	        	<h1 class="form-page__title" v-show="step == 3">{{ $t('ready') }}</h1>
+        	<div class="form-page__card__content">
+	    		<div class="form-page__container">
+		        	<h1 class="form-page__title" v-show="step == 1">{{ $t('usedEmail') }}</h1>
+		        	<h1 class="form-page__title" v-show="step == 2">{{ $t('sentCode') }}</h1>
+		        	<h1 class="form-page__title" v-show="step == 3">{{ $t('ready') }}</h1>
+		        </div>
+	        	<form class="form-page__form">
+	        		<div class="form-page__container">
+		        		<input class="form-page__input" name="email" type="email" :placeholder="$t('login')" required v-if="step == 1">
+		        		<input class="form-page__input" name="code" type="text" :placeholder="$t('writeCode')" required v-if="step == 2">
+		        		<input class="form-page__input" name="password" type="password" :placeholder="$t('newPassword')" required v-if="step == 3">
+		        		<input class="form-page__input" name="password-again" type="password" :placeholder="$t('repeatPassword')" required v-if="step == 3">
+		        		<clink :to="'/login'" class="form-page__text">{{ $t('LogIn') }}</clink>
+		        		<clink :to="'/register'" class="form-page__text double">{{ $t('noAccount') }} <span class="blue double-second">{{ $t('signUp') }}</span></clink>
+		        		<div class="form-page__enter-with">
+		        			<span class="form-page__text">{{ $t('signInBy') }}</span>
+		        			<clink :to="''" class="form-page__social">
+                                <img src="~/static/svg/form-page/fb.svg" />
+                            </clink>
+                            <clink :to="''" class="form-page__social">
+                                <img src="~/static/svg/form-page/ig.svg" />
+                            </clink>
+		        		</div>
+		        	</div>
+	        		<button class="form-page__submit" @click="goToNextStep()">{{ $t('send') }}</button>
+	        	</form>
 	        </div>
-        	<form class="form-page__form">
-        		<div class="form-page__container">
-	        		<input class="form-page__input" name="email" type="email" :placeholder="$t('login')" required v-if="step == 1">
-	        		<input class="form-page__input" name="code" type="text" :placeholder="$t('writeCode')" required v-if="step == 2">
-	        		<input class="form-page__input" name="password" type="password" :placeholder="$t('newPassword')" required v-if="step == 3">
-	        		<input class="form-page__input" name="password-again" type="password" :placeholder="$t('repeatPassword')" required v-if="step == 3">
-	        		<clink :to="'/login'" class="form-page__text">{{ $t('LogIn') }}</clink>
-	        		<clink :to="'/register'" class="form-page__text double">{{ $t('noAccount') }} <span class="blue double-second">{{ $t('signUp') }}</span></clink>
-	        		<div class="form-page__enter-with">
-	        			<span class="form-page__text">{{ $t('signInBy') }}</span>
-	        			<clink :to="'https://facebook.com/'" class="form-page__social">
-	        				<svg-icon name="form-page/fb.svg" />
-	        			</clink>
-	        			<clink :to="'https://instagram.com/'" class="form-page__social">
-	        				<svg-icon name="form-page/ig.svg" />
-	        			</clink>
-	        		</div>
-	        	</div>
-        		<button class="form-page__submit" @click="goToNextStep()">{{ $t('send') }}</button>
-        	</form>
         </div>
     </div>
 </template>
