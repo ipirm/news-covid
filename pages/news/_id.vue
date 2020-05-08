b8f9bee3dad487cd61258bbcfe6c67f2789dcbd1
Ilham <i.pirmamadov.synapse.company@gmail.com>
2020-05-09 00:53:38 +0400

    Fixed Created

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="news-inner-page">
-        <adsbygoogle class="page__content custom-container custom-container--news news-page-item" ad-slot="coronavirus" />
+<!--        <adsbygoogle class="page__content custom-container custom-container&#45;&#45;news news-page-item" ad-slot="coronavirus" />-->
         <div class="page__content custom-container custom-container--news news-page-item">
             <main class="page__double-main" v-for="(activeNews,index) in activeNews" :key="index"
                   style="margin-right: 20px;max-width: 950px; margin-left: auto;">
@@ -38,6 +38,7 @@
                             </clink>
                         </div>
                         <div class="news-content-date-item">
+                            {{ activeNews.created_at | moment("from", "now")  }}
                         </div>
                         <div class="news-content-date-item">
                             <clink :to="`/news?country=${activeNews.country[$i18n.locale]}`">{{
